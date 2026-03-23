e6bf19e0b5136b6f07626bc98d881e6d3772db2d
MarcoEscobar1 <marco.escobar1027@gmail.com>
2026-03-23 09:53:08 -0400

    Update: Creacion del archivo config para el manejo de secciones

---
+++ src/config/constants.js
@@ -0,0 +1,13 @@
+export const WHATSAPP_NUMBER = "59169536433";
+export const API_BASE_URL = "http://localhost:4000";
+
+export const API_ENDPOINTS = {
+  products: `${API_BASE_URL}/api/products`,
+};
+
+export const SECTION_IDS = {
+  individual: "individual",
+  porcion: "porcion",
+  combos: "combos",
+  pedidos: "pedidos",
+};
